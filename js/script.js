17578a04831f3ac2faf9afdd1c6ab48f1dc291b8
yusong-shen <yusongtju@gmail.com>
2016-06-29 21:58:46 -0500

    load related nyt article about input city through nyt api

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function loadData() {
     var $greeting = $('#greeting');
 
     var google_key = 'AIzaSyDxiC5gPdIQAFuo3r-KujeRYKLQEL7dUNs';
+    var nyt_key = 'a19e123ecd362cfd3ecdca1102dc45ee:13:75095583';
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -22,6 +23,21 @@ function loadData() {
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    // load new york time article
+    var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr+ '&sort=newest&api-key=' + nyt_key;
+
+    $.getJSON(nytURL, function (data){
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+ '<a href="' + article.web_url + '">' + article.headline.main + '</a>'+
+                '<p>' + article.snippet + '</p>' + 
+                '</li>');
+        };
+    });
+    
     return false;
 };
 
